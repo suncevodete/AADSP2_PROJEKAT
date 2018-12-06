add06ffa3a043e662f9eebb4a99be3dec3601d1b
student <student@domain.local>
2018-12-06 14:44:52 +0100

    radi skripta bez modela 3git add .git add .git add .!

--- Day_1_model2/aadsp2_projekat/Test.bat
+++ Day_1_model2/aadsp2_projekat/Test.bat
@@ -1,49 +1,34 @@
-:Delete log files first/
-cd OutCmp
-del output1_Model0_vs_Model1.txt
-del output1_Model1_vs_Model2.txt
-del output2_Model1_vs_Model3.txt
+@echo off
 
-:Executue model_0 model_1 model_2
-cd ..
+SET CONFIGURATION=Debug
+SET PROJECT_NAME=aadsp2_projekat
 
-cd model_0\Debug
+: TO DO: Add additional command line args for your program
+SET ADDITIONAL_ARGS=320" "0.8912509381337456
 
-"model_0.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\speech_2ch_2.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model0_output_speech_2ch_2.wav" "320" "0.8912509381337456"
-
-"model_1.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\speech_2ch_2.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_speech_2ch_2.wav" "320" "0.8912509381337456"
-
-"model_2.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\speech_2ch_2.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model2_output_speech_2ch_2.wav" "320" "0.8912509381337456"
+SET SIMULATOR=C:\CirrusDSP\bin\cmdline_simulator.exe -silent
+SET COMPARE=PCMCompare.exe
 
 
+: Delete log files first.
 
-"model_0.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\2ch_lvl_amt_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model0_output_2ch_lvl_amt_48k.wav" "320" "0.8912509381337456"
-
-"model_1.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\2ch_lvl_amt_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_2ch_lvl_amt_48k.wav" "320" "0.8912509381337456"
-
-"model_2.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\2ch_lvl_amt_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model2_output_2ch_lvl_amt_48k.wav" "320" "0.8912509381337456"
+del /Q OutCmp\*
+del /Q OutStreams\*
 
 
-"model_0.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\2ch_contour_ne40_24b_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model0_output_2ch_contour_ne40_24b_48k.wav" "320" "0.8912509381337456"
+: For each Test stream
+for %%f in (TestStreams\*.*) do ( 
+	@echo Running tests for stream: %%~nf%%~xf
 
-"model_1.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\2ch_contour_ne40_24b_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_2ch_contour_ne40_24b_48k.wav" "320" "0.8912509381337456"
+	: Execute Model 0, Model 1 and Model 2
+	echo     model 0
+	"model_0\Debug\model_0.exe" "%%f" "OutStreams//%%~nf_model0.wav" "%ADDITIONAL_ARGS%"
+	echo     model 1
+	"model_1\Debug\model_1.exe" "%%f" "OutStreams//%%~nf_model1.wav" "%ADDITIONAL_ARGS%"
+	echo     model 2
+	"model_2\Debug\model_2.exe" "%%f" "OutStreams//%%~nf_model2.wav" "%ADDITIONAL_ARGS%"
 
-"model_2.exe" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\TestStreams\2ch_contour_ne40_24b_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model2_output_2ch_contour_ne40_24b_48k.wav" "320" "0.8912509381337456"
-
-
-
-cd ..\..
-:Generate new logs
-
-
-PCMCompare "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model0_output_2ch_contour_ne40_24b_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_2ch_contour_ne40_24b_48k.wav" > C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutCmp\Model0_vs_Model1_2ch_contour_ne40_24b_48k.txt
-
-PCMCompare "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_2ch_contour_ne40_24b_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model2_output_2ch_contour_ne40_24b_48k.wav" > C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutCmp\Model1_vs_Model2_2ch_contour_ne40_24b_48k.txt
-
-PCMCompare "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model0_output_2ch_lvl_amt_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_2ch_lvl_amt_48k.wav" > C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutCmp\Model0_vs_Model1_2ch_lvl_amt_48k.txt
-
-PCMCompare "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_2ch_lvl_amt_48k.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model2_output_2ch_lvl_amt_48k.wav" > C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutCmp\Model1_vs_Model2_2ch_lvl_amt_48k.txt
-
-PCMCompare "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model0_output_speech_2ch_2.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_speech_2ch_2.wav" > C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutCmp\Model0_vs_Model1_model0_output_speech_2ch_2.txt
-
-PCMCompare "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model1_output_speech_2ch_2.wav" "C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutputStreams\model2_output_speech_2ch_2.wav" > C:\Users\student\Desktop\ra22-2015\AADSP2_PROJEKAT\Day_1_model2\aadsp2_projekat\OutCmp\Model1_vs_Model2_model0_output_speech_2ch_2.txt
+	: Generate new logs
+	%COMPARE% OutStreams//%%~nf_model0.wav OutStreams//%%~nf_model1.wav 2> OutCmp//%%~nf_Model0_vs_Model1.txt
+	%COMPARE% OutStreams//%%~nf_model1.wav OutStreams//%%~nf_model2.wav 2> OutCmp//%%~nf_Model1_vs_Model2.txt
+)
